d53fcef835382a20082020cbbc550552d922ba9e
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-07-28 01:10:35 -0700

    Fix CQS signal readability-braces-around-statements in fbcode/kudu/consensus
    Reviewed By: dtolnay
    
    Differential Revision: D79068852
    
    fbshipit-source-id: 87b42da92f1e91f7b89b83e41c1538f4b1083033

--- src/kudu/consensus/leader_election-test.cc
+++ src/kudu/consensus/leader_election-test.cc
@@ -108,8 +108,9 @@ class FromMapPeerProxyFactory : public PeerProxyFactory {
   Status NewProxy(const RaftPeerPB& peer_pb, shared_ptr<PeerProxy>* proxy)
       override {
     PeerProxy* proxy_ptr = FindPtrOrNull(*proxy_map_, peer_pb.permanent_uuid());
-    if (!proxy_ptr)
+    if (!proxy_ptr) {
       return Status::NotFound("no proxy for peer");
+    }
     proxy->reset(proxy_ptr);
     return Status::OK();
   }
