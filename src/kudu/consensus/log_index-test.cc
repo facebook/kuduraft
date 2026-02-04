c609a018e5946833e395388a312ebf7b0a751b21
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2026-02-03 16:35:56 -0800

    fbcode/kudu/consensus
    Reviewed By: yichenshen
    
    Differential Revision: D92141896
    
    fbshipit-source-id: 64aded1288a8c087324f196d2c7e54235b807dee

--- src/kudu/consensus/log_index-test.cc
+++ src/kudu/consensus/log_index-test.cc
@@ -41,25 +41,25 @@ class LogIndexTest : public KuduTest {
   }
 
  protected:
-  Status AddEntry(const OpId& op_id, int64_t segment, int64_t offset) {
+  Status addEntry(const OpId& opId, int64_t segment, int64_t offset) {
     LogIndexEntry entry;
-    entry.op_id = op_id;
+    entry.op_id = opId;
     entry.segment_sequence_number = segment;
     entry.offset_in_segment = offset;
     return index_->AddEntry(entry);
   }
 
-  void VerifyEntry(const OpId& op_id, int64_t segment, int64_t offset) {
-    SCOPED_TRACE(op_id);
+  void verifyEntry(const OpId& opId, int64_t segment, int64_t offset) {
+    SCOPED_TRACE(opId);
     LogIndexEntry result;
-    EXPECT_OK(index_->GetEntry(op_id.index(), &result));
-    EXPECT_EQ(op_id.term(), result.op_id.term());
-    EXPECT_EQ(op_id.index(), result.op_id.index());
+    EXPECT_OK(index_->GetEntry(opId.index(), &result));
+    EXPECT_EQ(opId.term(), result.op_id.term());
+    EXPECT_EQ(opId.index(), result.op_id.index());
     EXPECT_EQ(segment, result.segment_sequence_number);
     EXPECT_EQ(offset, result.offset_in_segment);
   }
 
-  void VerifyNotFound(int64_t index) {
+  void verifyNotFound(int64_t index) {
     SCOPED_TRACE(index);
     LogIndexEntry result;
     Status s = index_->GetEntry(index, &result);
@@ -71,48 +71,48 @@ class LogIndexTest : public KuduTest {
 
 TEST_F(LogIndexTest, TestBasic) {
   // Insert three entries.
-  ASSERT_OK(AddEntry(MakeOpId(1, 1), 1, 12345));
-  ASSERT_OK(AddEntry(MakeOpId(1, 999999), 1, 999));
-  ASSERT_OK(AddEntry(MakeOpId(1, 1500000), 1, 54321));
-  VerifyEntry(MakeOpId(1, 1), 1, 12345);
-  VerifyEntry(MakeOpId(1, 999999), 1, 999);
-  VerifyEntry(MakeOpId(1, 1500000), 1, 54321);
+  ASSERT_OK(addEntry(MakeOpId(1, 1), 1, 12345));
+  ASSERT_OK(addEntry(MakeOpId(1, 999999), 1, 999));
+  ASSERT_OK(addEntry(MakeOpId(1, 1500000), 1, 54321));
+  verifyEntry(MakeOpId(1, 1), 1, 12345);
+  verifyEntry(MakeOpId(1, 999999), 1, 999);
+  verifyEntry(MakeOpId(1, 1500000), 1, 54321);
 
   // Overwrite one.
-  ASSERT_OK(AddEntry(MakeOpId(5, 1), 1, 50000));
-  VerifyEntry(MakeOpId(5, 1), 1, 50000);
+  ASSERT_OK(addEntry(MakeOpId(5, 1), 1, 50000));
+  verifyEntry(MakeOpId(5, 1), 1, 50000);
 }
 
 TEST_F(LogIndexTest, TestMultiSegmentWithGC) {
-  ASSERT_OK(AddEntry(MakeOpId(1, 1), 1, 12345));
-  ASSERT_OK(AddEntry(MakeOpId(1, 1000000), 1, 54321));
-  ASSERT_OK(AddEntry(MakeOpId(1, 1500000), 1, 54321));
-  ASSERT_OK(AddEntry(MakeOpId(1, 2500000), 1, 12345));
+  ASSERT_OK(addEntry(MakeOpId(1, 1), 1, 12345));
+  ASSERT_OK(addEntry(MakeOpId(1, 1000000), 1, 54321));
+  ASSERT_OK(addEntry(MakeOpId(1, 1500000), 1, 54321));
+  ASSERT_OK(addEntry(MakeOpId(1, 2500000), 1, 12345));
 
   // GCing indexes < 1,000,000 shouldn't have any effect, because we can't
   // remove any whole segment.
   for (int gc = 0; gc < 1000000; gc += 100000) {
     SCOPED_TRACE(gc);
     index_->GC(gc);
-    VerifyEntry(MakeOpId(1, 1), 1, 12345);
-    VerifyEntry(MakeOpId(1, 1000000), 1, 54321);
-    VerifyEntry(MakeOpId(1, 1500000), 1, 54321);
-    VerifyEntry(MakeOpId(1, 2500000), 1, 12345);
+    verifyEntry(MakeOpId(1, 1), 1, 12345);
+    verifyEntry(MakeOpId(1, 1000000), 1, 54321);
+    verifyEntry(MakeOpId(1, 1500000), 1, 54321);
+    verifyEntry(MakeOpId(1, 2500000), 1, 12345);
   }
 
   // If we GC index 1000000, we should lose the first op.
   index_->GC(1000000);
-  VerifyNotFound(1);
-  VerifyEntry(MakeOpId(1, 1000000), 1, 54321);
-  VerifyEntry(MakeOpId(1, 1500000), 1, 54321);
-  VerifyEntry(MakeOpId(1, 2500000), 1, 12345);
+  verifyNotFound(1);
+  verifyEntry(MakeOpId(1, 1000000), 1, 54321);
+  verifyEntry(MakeOpId(1, 1500000), 1, 54321);
+  verifyEntry(MakeOpId(1, 2500000), 1, 12345);
 
   // GC everything
   index_->GC(9000000);
-  VerifyNotFound(1);
-  VerifyNotFound(1000000);
-  VerifyNotFound(1500000);
-  VerifyNotFound(2500000);
+  verifyNotFound(1);
+  verifyNotFound(1000000);
+  verifyNotFound(1500000);
+  verifyNotFound(2500000);
 }
 
 TEST(LogIndexEntry, Comparison) {
