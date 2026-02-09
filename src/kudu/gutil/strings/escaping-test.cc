592e28dfb304fc2266071f6eb542aede239bbd4d
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2026-02-09 07:07:24 -0800

    fbcode/kudu/gutil/strings
    Reviewed By: yichenshen
    
    Differential Revision: D92246332
    
    fbshipit-source-id: 34b411d5617b9365e391a93fcc20d142a5ae1cc0

--- src/kudu/gutil/strings/escaping-test.cc
+++ src/kudu/gutil/strings/escaping-test.cc
@@ -25,39 +25,39 @@ TEST(EscapingTest, Utf8SafeCEscapeWithHighBytes) {
   // when using Utf8SafeCEscape (that's the "Utf8Safe" part)
 
   // 2-byte UTF-8: U+00E9 (é) = 0xC3 0xA9
-  std::string utf8_e_acute = "\xc3\xa9";
-  EXPECT_EQ(utf8_e_acute, Utf8SafeCEscape(utf8_e_acute));
+  std::string utf8EAcute = "\xc3\xa9";
+  EXPECT_EQ(utf8EAcute, Utf8SafeCEscape(utf8EAcute));
 
   // 3-byte UTF-8: U+2660 (♠) = 0xE2 0x99 0xA0
-  std::string utf8_spade = "\xe2\x99\xa0";
-  EXPECT_EQ(utf8_spade, Utf8SafeCEscape(utf8_spade));
+  std::string utf8Spade = "\xe2\x99\xa0";
+  EXPECT_EQ(utf8Spade, Utf8SafeCEscape(utf8Spade));
 
   // 4-byte UTF-8: U+1F600 (😀) = 0xF0 0x9F 0x98 0x80
-  std::string utf8_grin = "\xf0\x9f\x98\x80";
-  EXPECT_EQ(utf8_grin, Utf8SafeCEscape(utf8_grin));
+  std::string utf8Grin = "\xf0\x9f\x98\x80";
+  EXPECT_EQ(utf8Grin, Utf8SafeCEscape(utf8Grin));
 
   // Mixed ASCII and UTF-8
   std::string mixed = "Hello \xe2\x99\xa0 World";
   EXPECT_EQ(mixed, Utf8SafeCEscape(mixed));
 
   // UTF-8 with control chars that need escaping
-  std::string utf8_with_newline = "\xe2\x99\xa0\n\xe2\x99\xa0";
-  EXPECT_EQ("\xe2\x99\xa0\\n\xe2\x99\xa0", Utf8SafeCEscape(utf8_with_newline));
+  std::string utf8WithNewline = "\xe2\x99\xa0\n\xe2\x99\xa0";
+  EXPECT_EQ("\xe2\x99\xa0\\n\xe2\x99\xa0", Utf8SafeCEscape(utf8WithNewline));
 
   // Utf8SafeCEscape should preserve high bytes (treating as UTF-8 continuation)
-  std::string high_byte = "\x80";
-  EXPECT_EQ(high_byte, Utf8SafeCEscape(high_byte));
+  std::string highByte = "\x80";
+  EXPECT_EQ(highByte, Utf8SafeCEscape(highByte));
 
   // Byte at 0xff boundary
-  std::string max_byte = "\xff";
-  EXPECT_EQ(max_byte, Utf8SafeCEscape(max_byte));
+  std::string maxByte = "\xff";
+  EXPECT_EQ(maxByte, Utf8SafeCEscape(maxByte));
 }
 
 // Test edge case: byte value 0x7F (DEL) - just below 0x80 threshold
 TEST(EscapingTest, Utf8SafeCEscapeBoundary) {
   // 0x7F (DEL) is not printable, should be escaped
-  std::string del_char = "\x7f";
-  EXPECT_NE(del_char, Utf8SafeCEscape(del_char));
+  std::string delChar = "\x7f";
+  EXPECT_NE(delChar, Utf8SafeCEscape(delChar));
 
   // 0x80 is at the boundary - should be preserved by Utf8SafeCEscape
   std::string boundary = "\x80";
