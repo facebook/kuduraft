378f79830c28cce7dbae4e859f4f81fb43956104
generatedunixname1418253959772185 <noreply+1418253959772185@fb.com>
2026-02-09 12:23:09 -0800

    fbcode/kudu/util
    Reviewed By: yichenshen
    
    Differential Revision: D92688848
    
    fbshipit-source-id: 6a50e7dbaa812dfdd82666d7e7000c984e5e42b4

--- src/kudu/util/atomic-test.cc
+++ src/kudu/util/atomic-test.cc
@@ -40,11 +40,11 @@ class AtomicIntTest : public KuduTest {
  public:
   AtomicIntTest()
       : max_(numeric_limits<T>::max()), min_(numeric_limits<T>::min()) {
-    acquire_release_ = {kMemOrderNoBarrier, kMemOrderAcquire, kMemOrderRelease};
+    acquireRelease_ = {kMemOrderNoBarrier, kMemOrderAcquire, kMemOrderRelease};
     barrier_ = {kMemOrderNoBarrier, kMemOrderBarrier};
   }
 
-  vector<MemoryOrder> acquire_release_;
+  vector<MemoryOrder> acquireRelease_;
   vector<MemoryOrder> barrier_;
 
   T max_;
@@ -55,79 +55,79 @@ using IntTypes = ::testing::Types<int32_t, int64_t, uint32_t, uint64_t>;
 TYPED_TEST_CASE(AtomicIntTest, IntTypes);
 
 TYPED_TEST(AtomicIntTest, LoadStore) {
-  for (const MemoryOrder mem_order : this->acquire_release_) {
+  for (const MemoryOrder memOrder : this->acquireRelease_) {
     AtomicInt<TypeParam> i(0);
-    EXPECT_EQ(0, i.Load(mem_order));
-    i.Store(42, mem_order);
-    EXPECT_EQ(42, i.Load(mem_order));
-    i.Store(this->min_, mem_order);
-    EXPECT_EQ(this->min_, i.Load(mem_order));
-    i.Store(this->max_, mem_order);
-    EXPECT_EQ(this->max_, i.Load(mem_order));
+    EXPECT_EQ(0, i.Load(memOrder));
+    i.Store(42, memOrder);
+    EXPECT_EQ(42, i.Load(memOrder));
+    i.Store(this->min_, memOrder);
+    EXPECT_EQ(this->min_, i.Load(memOrder));
+    i.Store(this->max_, memOrder);
+    EXPECT_EQ(this->max_, i.Load(memOrder));
   }
 }
 
 TYPED_TEST(AtomicIntTest, SetSwapExchange) {
-  for (const MemoryOrder mem_order : this->acquire_release_) {
+  for (const MemoryOrder memOrder : this->acquireRelease_) {
     AtomicInt<TypeParam> i(0);
-    EXPECT_TRUE(i.CompareAndSet(0, 5, mem_order));
-    EXPECT_EQ(5, i.Load(mem_order));
-    EXPECT_FALSE(i.CompareAndSet(0, 10, mem_order));
+    EXPECT_TRUE(i.CompareAndSet(0, 5, memOrder));
+    EXPECT_EQ(5, i.Load(memOrder));
+    EXPECT_FALSE(i.CompareAndSet(0, 10, memOrder));
 
-    EXPECT_EQ(5, i.CompareAndSwap(5, this->max_, mem_order));
-    EXPECT_EQ(this->max_, i.CompareAndSwap(42, 42, mem_order));
-    EXPECT_EQ(this->max_, i.CompareAndSwap(this->max_, this->min_, mem_order));
+    EXPECT_EQ(5, i.CompareAndSwap(5, this->max_, memOrder));
+    EXPECT_EQ(this->max_, i.CompareAndSwap(42, 42, memOrder));
+    EXPECT_EQ(this->max_, i.CompareAndSwap(this->max_, this->min_, memOrder));
 
-    EXPECT_EQ(this->min_, i.Exchange(this->max_, mem_order));
-    EXPECT_EQ(this->max_, i.Load(mem_order));
+    EXPECT_EQ(this->min_, i.Exchange(this->max_, memOrder));
+    EXPECT_EQ(this->max_, i.Load(memOrder));
   }
 }
 
 TYPED_TEST(AtomicIntTest, MinMax) {
-  for (const MemoryOrder mem_order : this->acquire_release_) {
+  for (const MemoryOrder memOrder : this->acquireRelease_) {
     AtomicInt<TypeParam> i(0);
 
-    i.StoreMax(100, mem_order);
-    EXPECT_EQ(100, i.Load(mem_order));
-    i.StoreMin(50, mem_order);
-    EXPECT_EQ(50, i.Load(mem_order));
+    i.StoreMax(100, memOrder);
+    EXPECT_EQ(100, i.Load(memOrder));
+    i.StoreMin(50, memOrder);
+    EXPECT_EQ(50, i.Load(memOrder));
 
-    i.StoreMax(25, mem_order);
-    EXPECT_EQ(50, i.Load(mem_order));
-    i.StoreMin(75, mem_order);
-    EXPECT_EQ(50, i.Load(mem_order));
+    i.StoreMax(25, memOrder);
+    EXPECT_EQ(50, i.Load(memOrder));
+    i.StoreMin(75, memOrder);
+    EXPECT_EQ(50, i.Load(memOrder));
 
-    i.StoreMax(this->max_, mem_order);
-    EXPECT_EQ(this->max_, i.Load(mem_order));
-    i.StoreMin(this->min_, mem_order);
-    EXPECT_EQ(this->min_, i.Load(mem_order));
+    i.StoreMax(this->max_, memOrder);
+    EXPECT_EQ(this->max_, i.Load(memOrder));
+    i.StoreMin(this->min_, memOrder);
+    EXPECT_EQ(this->min_, i.Load(memOrder));
   }
 }
 
 TYPED_TEST(AtomicIntTest, Increment) {
-  for (const MemoryOrder mem_order : this->barrier_) {
+  for (const MemoryOrder memOrder : this->barrier_) {
     AtomicInt<TypeParam> i(0);
-    EXPECT_EQ(1, i.Increment(mem_order));
-    EXPECT_EQ(3, i.IncrementBy(2, mem_order));
-    EXPECT_EQ(3, i.IncrementBy(0, mem_order));
+    EXPECT_EQ(1, i.Increment(memOrder));
+    EXPECT_EQ(3, i.IncrementBy(2, memOrder));
+    EXPECT_EQ(3, i.IncrementBy(0, memOrder));
   }
 }
 
 TEST(Atomic, AtomicBool) {
-  vector<MemoryOrder> memory_orders = {
+  vector<MemoryOrder> memoryOrders = {
       kMemOrderNoBarrier, kMemOrderRelease, kMemOrderAcquire};
-  for (const MemoryOrder mem_order : memory_orders) {
+  for (const MemoryOrder memOrder : memoryOrders) {
     AtomicBool b(false);
-    EXPECT_FALSE(b.Load(mem_order));
-    b.Store(true, mem_order);
-    EXPECT_TRUE(b.Load(mem_order));
-    EXPECT_TRUE(b.CompareAndSet(true, false, mem_order));
-    EXPECT_FALSE(b.Load(mem_order));
-    EXPECT_FALSE(b.CompareAndSet(true, false, mem_order));
-    EXPECT_FALSE(b.CompareAndSwap(false, true, mem_order));
-    EXPECT_TRUE(b.Load(mem_order));
-    EXPECT_TRUE(b.Exchange(false, mem_order));
-    EXPECT_FALSE(b.Load(mem_order));
+    EXPECT_FALSE(b.Load(memOrder));
+    b.Store(true, memOrder);
+    EXPECT_TRUE(b.Load(memOrder));
+    EXPECT_TRUE(b.CompareAndSet(true, false, memOrder));
+    EXPECT_FALSE(b.Load(memOrder));
+    EXPECT_FALSE(b.CompareAndSet(true, false, memOrder));
+    EXPECT_FALSE(b.CompareAndSwap(false, true, memOrder));
+    EXPECT_TRUE(b.Load(memOrder));
+    EXPECT_TRUE(b.Exchange(false, memOrder));
+    EXPECT_FALSE(b.Load(memOrder));
   }
 }
 
