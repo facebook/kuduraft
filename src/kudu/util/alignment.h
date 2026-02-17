13e64bc92bcc592c702de372f1693d9712fb334d
generatedunixname1566653597922694 <noreply+1566653597922694@fb.com>
2026-02-17 15:46:11 -0800

    fbcode/kudu/util
    Reviewed By: yichenshen
    
    Differential Revision: D93458810
    
    fbshipit-source-id: 36999b6290972e2e6455972bee7401d513f8c14f

--- src/kudu/util/alignment.h
+++ src/kudu/util/alignment.h
@@ -19,9 +19,6 @@
 #ifndef KUDU_UTIL_ALIGNMENT_H
 #define KUDU_UTIL_ALIGNMENT_H
 
-// Round down 'x' to the nearest 'align' boundary
-#define KUDU_ALIGN_DOWN(x, align) ((x) & (~(align) + 1))
-
 // Round up 'x' to the nearest 'align' boundary
 #define KUDU_ALIGN_UP(x, align) (((x) + ((align) - 1)) & (~(align) + 1))
 
