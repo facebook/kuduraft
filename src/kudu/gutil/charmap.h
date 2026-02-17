4fe48282c20b9e0b12a33d692a80b4bd5770559f
generatedunixname1566653597922694 <noreply+1566653597922694@fb.com>
2026-02-17 14:22:35 -0800

    fbcode/kudu/gutil
    Reviewed By: yichenshen
    
    Differential Revision: D93076348
    
    fbshipit-source-id: d9034f95a45fc7e6cdf383713fe71b139350a0e3

--- src/kudu/gutil/charmap.h
+++ src/kudu/gutil/charmap.h
@@ -23,33 +23,6 @@
 
 class Charmap {
  public:
-  // Initializes with given uint32_t values.  For instance, the first
-  // variable contains bits for values 0x1F (US) down to 0x00 (NUL).
-  Charmap(
-      uint32_t b0,
-      uint32_t b1,
-      uint32_t b2,
-      uint32_t b3,
-      uint32_t b4,
-      uint32_t b5,
-      uint32_t b6,
-      uint32_t b7) {
-    m_[0] = b0;
-    m_[1] = b1;
-    m_[2] = b2;
-    m_[3] = b3;
-    m_[4] = b4;
-    m_[5] = b5;
-    m_[6] = b6;
-    m_[7] = b7;
-  }
-
-  // Initializes with a given char*.  Note that NUL is not treated as
-  // a terminator, but rather a char to be flicked.
-  Charmap(const char* str, int len) {
-    Init(str, len);
-  }
-
   // Initializes with a given char*.  NUL is treated as a terminator
   // and will not be in the charmap.
   explicit Charmap(const char* str) {
@@ -58,25 +31,6 @@ class Charmap {
 
   bool contains(unsigned char c) const {
     return (m_[c >> 5] >> (c & 0x1f)) & 0x1;
-  }
-
-  // Returns true if and only if a character exists in both maps.
-  bool IntersectsWith(const Charmap& c) const {
-    for (int i = 0; i < 8; ++i) {
-      if ((m_[i] & c.m_[i]) != 0) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool IsZero() const {
-    for (uint32_t c : m_) {
-      if (c != 0) {
-        return false;
-      }
-    }
-    return true;
   }
 
  protected:
