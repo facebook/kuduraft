ada4a34e119403d90b64c9e11d4d23e71d69c871
generatedunixname1418253959772185 <noreply+1418253959772185@fb.com>
2026-02-19 16:16:26 -0800

    fbcode/kudu/tools
    Reviewed By: yichenshen
    
    Differential Revision: D93711372
    
    fbshipit-source-id: c6115e31fee20c894eb02d56101a0a897882a5be

--- src/kudu/tools/diagnostics_log_parser.cc
+++ src/kudu/tools/diagnostics_log_parser.cc
@@ -82,7 +82,7 @@ void StackDumpingLogVisitor::visitStacksRecord(const StacksRecord& sr) {
          << "]" << endl;
     for (const auto& addr : group.frameAddrs) {
       auto it = symbols_.find(addr);
-      const auto& sym = (it != symbols_.end()) ? it->second : kUnknownSymbol;
+      const auto& sym = (it != symbols_.end()) ? it->second : unknownSymbol_;
       cout << std::setw(20) << addr << " " << sym << endl;
     }
   }
