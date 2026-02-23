0ab6365ce4eebaeb08929e515b371923239d8b71
generatedunixname1566653597922694 <noreply+1566653597922694@fb.com>
2026-02-23 11:05:23 -0800

    fbcode/kudu/gutil/hash
    Reviewed By: yichenshen
    
    Differential Revision: D94053988
    
    fbshipit-source-id: ebb4ac14d45c7b7bb4b7680fc4dcf45e1d3e5358

--- src/kudu/gutil/hash/string_hash.h
+++ src/kudu/gutil/hash/string_hash.h
@@ -37,20 +37,6 @@ static const uint64_t kMix64 = 0x2b992ddfa23249d6ULL;
 
 } // namespace hash_internal
 
-inline size_t
-hashStringThoroughlyWithSeed(const char* s, size_t len, size_t seed) {
-  if (hash_internal::kX8664) {
-    return static_cast<size_t>(util_hash::cityHash64WithSeed(s, len, seed));
-  }
-
-  if (hash_internal::kSixtyFourBit) {
-    return hash64StringWithSeed(s, static_cast<uint32_t>(len), seed);
-  }
-
-  return static_cast<size_t>(hash32StringWithSeed(
-      s, static_cast<uint32_t>(len), static_cast<uint32_t>(seed)));
-}
-
 inline size_t hashStringThoroughly(const char* s, size_t len) {
   if (hash_internal::kX8664) {
     return static_cast<size_t>(util_hash::cityHash64(s, len));
@@ -63,28 +49,4 @@ inline size_t hashStringThoroughly(const char* s, size_t len) {
 
   return static_cast<size_t>(hash32StringWithSeed(
       s, static_cast<uint32_t>(len), hash_internal::kMix32));
-}
-
-inline size_t hashStringThoroughlyWithSeeds(
-    const char* s,
-    size_t len,
-    size_t seed0,
-    size_t seed1) {
-  if (hash_internal::kX8664) {
-    return util_hash::cityHash64WithSeeds(s, len, seed0, seed1);
-  }
-
-  if (hash_internal::kSixtyFourBit) {
-    uint64_t a = seed0;
-    uint64_t b = seed1;
-    uint64_t c = hashStringThoroughly(s, len);
-    mix(a, b, c);
-    return c;
-  }
-
-  uint32_t a = static_cast<uint32_t>(seed0);
-  uint32_t b = static_cast<uint32_t>(seed1);
-  uint32_t c = static_cast<uint32_t>(hashStringThoroughly(s, len));
-  mix(a, b, c);
-  return c;
 }
