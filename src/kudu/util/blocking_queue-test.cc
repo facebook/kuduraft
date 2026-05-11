fa88dcd6da2ad0e0fa97e0dfbaed37fe4331c496
generatedunixname1418253959772185 <noreply+1418253959772185@fb.com>
2026-05-11 10:39:06 -0700

    fbcode/kudu/util
    Reviewed By: yichenshen
    
    Differential Revision: D104631389
    
    fbshipit-source-id: ea66322f66670afea214c5527df2661455150317

--- src/kudu/util/blocking_queue-test.cc
+++ src/kudu/util/blocking_queue-test.cc
@@ -37,46 +37,46 @@ using std::vector;
 
 namespace kudu {
 
-BlockingQueue<int32_t> test1_queue(5);
+BlockingQueue<int32_t> test1Queue(5);
 
-void InsertSomeThings() {
-  ASSERT_EQ(test1_queue.put(1), kQueueSuccess);
-  ASSERT_EQ(test1_queue.put(2), kQueueSuccess);
-  ASSERT_EQ(test1_queue.put(3), kQueueSuccess);
+void insertSomeThings() {
+  ASSERT_EQ(test1Queue.put(1), kQueueSuccess);
+  ASSERT_EQ(test1Queue.put(2), kQueueSuccess);
+  ASSERT_EQ(test1Queue.put(3), kQueueSuccess);
 }
 
 TEST(BlockingQueueTest, Test1) {
-  thread inserter_thread(InsertSomeThings);
+  thread inserterThread(insertSomeThings);
   int32_t i;
-  ASSERT_TRUE(test1_queue.blockingGet(&i));
+  ASSERT_TRUE(test1Queue.blockingGet(&i));
   ASSERT_EQ(1, i);
-  ASSERT_TRUE(test1_queue.blockingGet(&i));
+  ASSERT_TRUE(test1Queue.blockingGet(&i));
   ASSERT_EQ(2, i);
-  ASSERT_TRUE(test1_queue.blockingGet(&i));
+  ASSERT_TRUE(test1Queue.blockingGet(&i));
   ASSERT_EQ(3, i);
-  inserter_thread.join();
+  inserterThread.join();
 }
 
 TEST(BlockingQueueTest, TestBlockingDrainTo) {
-  BlockingQueue<int32_t> test_queue(3);
-  ASSERT_EQ(test_queue.put(1), kQueueSuccess);
-  ASSERT_EQ(test_queue.put(2), kQueueSuccess);
-  ASSERT_EQ(test_queue.put(3), kQueueSuccess);
+  BlockingQueue<int32_t> testQueue(3);
+  ASSERT_EQ(testQueue.put(1), kQueueSuccess);
+  ASSERT_EQ(testQueue.put(2), kQueueSuccess);
+  ASSERT_EQ(testQueue.put(3), kQueueSuccess);
   vector<int32_t> out;
-  ASSERT_OK(test_queue.blockingDrainTo(
+  ASSERT_OK(testQueue.blockingDrainTo(
       &out, MonoTime::Now() + MonoDelta::FromSeconds(30)));
   ASSERT_EQ(1, out[0]);
   ASSERT_EQ(2, out[1]);
   ASSERT_EQ(3, out[2]);
 
   // Set a deadline in the past and ensure we time out.
-  Status s = test_queue.blockingDrainTo(
+  Status s = testQueue.blockingDrainTo(
       &out, MonoTime::Now() - MonoDelta::FromSeconds(1));
   ASSERT_TRUE(s.IsTimedOut());
 
   // Ensure that if the queue is shut down, we get Aborted status.
-  test_queue.shutdown();
-  s = test_queue.blockingDrainTo(
+  testQueue.shutdown();
+  s = testQueue.blockingDrainTo(
       &out, MonoTime::Now() - MonoDelta::FromSeconds(1));
   ASSERT_TRUE(s.IsAborted());
 }
@@ -108,10 +108,10 @@ TEST(BlockingQueueTest, TestGetAndDrainAfterShutdown) {
 }
 
 TEST(BlockingQueueTest, TestTooManyInsertions) {
-  BlockingQueue<int32_t> test_queue(2);
-  ASSERT_EQ(test_queue.put(123), kQueueSuccess);
-  ASSERT_EQ(test_queue.put(123), kQueueSuccess);
-  ASSERT_EQ(test_queue.put(123), kQueueFull);
+  BlockingQueue<int32_t> testQueue(2);
+  ASSERT_EQ(testQueue.put(123), kQueueSuccess);
+  ASSERT_EQ(testQueue.put(123), kQueueSuccess);
+  ASSERT_EQ(testQueue.put(123), kQueueFull);
 }
 
 namespace {
@@ -125,15 +125,15 @@ struct LengthLogicalSize {
 } // anonymous namespace
 
 TEST(BlockingQueueTest, TestLogicalSize) {
-  BlockingQueue<string, LengthLogicalSize> test_queue(4);
-  ASSERT_EQ(test_queue.put("a"), kQueueSuccess);
-  ASSERT_EQ(test_queue.put("bcd"), kQueueSuccess);
-  ASSERT_EQ(test_queue.put("e"), kQueueFull);
+  BlockingQueue<string, LengthLogicalSize> testQueue(4);
+  ASSERT_EQ(testQueue.put("a"), kQueueSuccess);
+  ASSERT_EQ(testQueue.put("bcd"), kQueueSuccess);
+  ASSERT_EQ(testQueue.put("e"), kQueueFull);
 }
 
 TEST(BlockingQueueTest, TestNonPointerParamsMayBeNonEmptyOnDestruct) {
-  BlockingQueue<int32_t> test_queue(1);
-  ASSERT_EQ(test_queue.put(123), kQueueSuccess);
+  BlockingQueue<int32_t> testQueue(1);
+  ASSERT_EQ(testQueue.put(123), kQueueSuccess);
   // No DCHECK failure on destruct.
 }
 
@@ -142,9 +142,9 @@ TEST(BlockingQueueDeathTest, TestPointerParamsMustBeEmptyOnDestruct) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ASSERT_DEATH(
       {
-        BlockingQueue<int32_t*> test_queue(1);
+        BlockingQueue<int32_t*> testQueue(1);
         int32_t element = 123;
-        ASSERT_EQ(test_queue.put(&element), kQueueSuccess);
+        ASSERT_EQ(testQueue.put(&element), kQueueSuccess);
         // Debug assertion triggered on queue destruction since type is a
         // pointer.
       },
@@ -153,100 +153,100 @@ TEST(BlockingQueueDeathTest, TestPointerParamsMustBeEmptyOnDestruct) {
 #endif // NDEBUG
 
 TEST(BlockingQueueTest, TestGetFromShutdownQueue) {
-  BlockingQueue<int64_t> test_queue(2);
-  ASSERT_EQ(test_queue.put(123), kQueueSuccess);
-  test_queue.shutdown();
-  ASSERT_EQ(test_queue.put(456), kQueueShutdown);
+  BlockingQueue<int64_t> testQueue(2);
+  ASSERT_EQ(testQueue.put(123), kQueueSuccess);
+  testQueue.shutdown();
+  ASSERT_EQ(testQueue.put(456), kQueueShutdown);
   int64_t i;
-  ASSERT_TRUE(test_queue.blockingGet(&i));
+  ASSERT_TRUE(testQueue.blockingGet(&i));
   ASSERT_EQ(123, i);
-  ASSERT_FALSE(test_queue.blockingGet(&i));
+  ASSERT_FALSE(testQueue.blockingGet(&i));
 }
 
 TEST(BlockingQueueTest, TestGscopedPtrMethods) {
-  BlockingQueue<int*> test_queue(2);
-  std::unique_ptr<int> input_int(new int(123));
-  ASSERT_EQ(test_queue.put(&input_int), kQueueSuccess);
-  std::unique_ptr<int> output_int;
-  ASSERT_TRUE(test_queue.blockingGet(&output_int));
-  ASSERT_EQ(123, *output_int.get());
-  test_queue.shutdown();
+  BlockingQueue<int*> testQueue(2);
+  std::unique_ptr<int> inputInt(new int(123));
+  ASSERT_EQ(testQueue.put(&inputInt), kQueueSuccess);
+  std::unique_ptr<int> outputInt;
+  ASSERT_TRUE(testQueue.blockingGet(&outputInt));
+  ASSERT_EQ(123, *outputInt.get());
+  testQueue.shutdown();
 }
 
 class MultiThreadTest {
  public:
   MultiThreadTest()
-      : puts_(4),
-        blocking_puts_(4),
-        nthreads_(5),
-        queue_(nthreads_ * puts_),
-        num_inserters_(nthreads_),
-        sync_latch_(nthreads_) {}
+      : puts(4),
+        blockingPuts(4),
+        nthreads(5),
+        queue(nthreads * puts),
+        numInserters(nthreads),
+        syncLatch(nthreads) {}
 
-  void InserterThread(int arg) {
-    for (int i = 0; i < puts_; i++) {
-      ASSERT_EQ(queue_.put(arg), kQueueSuccess);
+  void inserterThread(int arg) {
+    for (int i = 0; i < puts; i++) {
+      ASSERT_EQ(queue.put(arg), kQueueSuccess);
     }
-    sync_latch_.countDown();
-    sync_latch_.wait();
-    for (int i = 0; i < blocking_puts_; i++) {
-      ASSERT_TRUE(queue_.blockingPut(arg));
+    syncLatch.countDown();
+    syncLatch.wait();
+    for (int i = 0; i < blockingPuts; i++) {
+      ASSERT_TRUE(queue.blockingPut(arg));
     }
-    MutexLock guard(lock_);
-    if (--num_inserters_ == 0) {
-      queue_.shutdown();
+    MutexLock guard(lock);
+    if (--numInserters == 0) {
+      queue.shutdown();
     }
   }
 
-  void RemoverThread() {
-    for (int i = 0; i < puts_ + blocking_puts_; i++) {
+  void removerThread() {
+    for (int i = 0; i < puts + blockingPuts; i++) {
       int32_t arg = 0;
-      bool got = queue_.blockingGet(&arg);
+      bool got = queue.blockingGet(&arg);
       if (!got) {
         arg = -1;
       }
-      MutexLock guard(lock_);
-      gotten_[arg] = gotten_[arg] + 1;
+      MutexLock guard(lock);
+      gotten[arg] = gotten[arg] + 1;
     }
   }
 
-  void Run() {
-    for (int i = 0; i < nthreads_; i++) {
-      threads_.emplace_back(&MultiThreadTest::InserterThread, this, i);
-      threads_.emplace_back(&MultiThreadTest::RemoverThread, this);
+  void run() {
+    for (int i = 0; i < nthreads; i++) {
+      threads.emplace_back(&MultiThreadTest::inserterThread, this, i);
+      threads.emplace_back(&MultiThreadTest::removerThread, this);
     }
     // We add an extra thread to ensure that there aren't enough elements in
     // the queue to go around.  This way, we test removal after shutdown.
-    threads_.emplace_back(&MultiThreadTest::RemoverThread, this);
-    for (auto& thread : threads_) {
+    threads.emplace_back(&MultiThreadTest::removerThread, this);
+    for (auto& thread : threads) {
       thread.join();
     }
     // Let's check to make sure we got what we should have.
-    MutexLock guard(lock_);
-    for (int i = 0; i < nthreads_; i++) {
-      ASSERT_EQ(puts_ + blocking_puts_, gotten_[i]);
+    MutexLock guard(lock);
+    for (int i = 0; i < nthreads; i++) {
+      ASSERT_EQ(puts + blockingPuts, gotten[i]);
     }
-    // And there were nthreads_ * (puts_ + blocking_puts_)
-    // elements removed, but only nthreads_ * puts_ +
-    // blocking_puts_ elements added.  So some removers hit the
+    // And there were nthreads * (puts + blockingPuts)
+    // elements removed, but only nthreads * puts +
+    // blockingPuts elements added.  So some removers hit the
     // shutdown case.
-    ASSERT_EQ(puts_ + blocking_puts_, gotten_[-1]);
+    ASSERT_EQ(puts + blockingPuts, gotten[-1]);
   }
 
-  int puts_;
-  int blocking_puts_;
-  int nthreads_;
-  BlockingQueue<int32_t> queue_;
-  Mutex lock_;
-  std::map<int32_t, int> gotten_;
-  vector<thread> threads_;
-  int num_inserters_;
-  CountDownLatch sync_latch_;
+  int puts;
+  int blockingPuts;
+  int nthreads;
+  BlockingQueue<int32_t> queue;
+  Mutex lock;
+  std::map<int32_t, int> gotten;
+  vector<thread> threads;
+  int numInserters;
+  CountDownLatch syncLatch;
 };
 
 TEST(BlockingQueueTest, TestMultipleThreads) {
   MultiThreadTest test;
-  test.Run();
+  test.run();
 }
 
 } // namespace kudu
