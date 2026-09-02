88a695380e8e3a194b194583ffddaf7455b0727b
generatedunixname1566653597922694 <noreply+1566653597922694@fb.com>
2026-09-01 21:06:37 -0700

    fbcode/kudu/gutil/strings
    Reviewed By: alanliang
    
    Differential Revision: D118116091
    
    fbshipit-source-id: 7e1e714213d26be552ee1461eed1cbc928d32366

--- src/kudu/gutil/strings/util.h
+++ src/kudu/gutil/strings/util.h
@@ -26,49 +26,12 @@
 #pragma once
 
 #include <cstddef>
-#include <cstdio>
-#include <cstring>
 
-#include <functional>
 #include <string>
-#include <vector>
 
-#include <cstdint>
-
-#include "kudu/gutil/port.h"
 #include "kudu/gutil/strings/stringpiece.h"
 
-// Newer functions.
-
-namespace strings {
-
-// Finds the next end-of-line sequence.
-// An end-of-line sequence is one of:
-//   \n    common on unix, including mac os x
-//   \r    common on macos 9 and before
-//   \r\n  common on windows
-//
-// Returns a StringPiece that contains the end-of-line sequence (a pointer into
-// the input, 1 or 2 characters long).
-//
-// If the input does not contain an end-of-line sequence, returns an empty
-// StringPiece located at the end of the input:
-//    StringPiece(sp.data() + sp.length(), 0).
-
-StringPiece findEol(StringPiece sp);
-
-} // namespace strings
-
 // Older functions.
-
-// Duplicates a non-null, non-empty char* string. Returns a pointer to the new
-// string, or NULL if the input is null or empty.
-inline char* strdupNonempty(const char* src) {
-  if (src && src[0]) {
-    return strdup(src);
-  }
-  return nullptr;
-}
 
 // Finds the first occurrence of a character in at most a given number of bytes
 // of a char* string. Returns a pointer to the first occurrence, or NULL if no
@@ -93,81 +56,6 @@ inline char* strnchr(const char* buf, char c, int sz) {
 // WARNING: Removes const-ness of string argument!
 char* strnstr(const char* haystack, const char* needle, size_t haystackLen);
 
-// Matches a prefix (which must be a char* literal!) against the beginning of
-// str. Returns a pointer past the prefix, or NULL if the prefix wasn't matched.
-// (Like the standard strcasecmp(), but for efficiency doesn't call strlen() on
-// prefix, and returns a pointer rather than an int.)
-//
-// The ""'s catch people who don't pass in a literal for "prefix"
-#ifndef strprefix
-#define strprefix(str, prefix)                         \
-  (strncmp(str, prefix, sizeof("" prefix "") - 1) == 0 \
-       ? (str) + sizeof(prefix) - 1                    \
-       : nullptr)
-#endif
-
-// Same as strprefix() (immediately above), but matches a case-insensitive
-// prefix.
-#ifndef strcaseprefix
-#define strcaseprefix(str, prefix)                         \
-  (strncasecmp(str, prefix, sizeof("" prefix "") - 1) == 0 \
-       ? (str) + sizeof(prefix) - 1                        \
-       : nullptr)
-#endif
-
-// Matches a prefix (up to the first needle_size bytes of needle) in the first
-// haystack_size byte of haystack. Returns a pointer past the prefix, or NULL if
-// the prefix wasn't matched. (Unlike strprefix(), prefix doesn't need to be a
-// char* literal. Like the standard strncmp(), but also takes a haystack_size,
-// and returns a pointer rather than an int.)
-//
-// Always returns either NULL or haystack + needle_size.
-//
-// Some windows header sometimes #defines strnprefix to something we
-// don't want.
-#ifdef strnprefix
-#undef strnprefix
-#endif
-const char* strnprefix(
-    const char* haystack,
-    int haystackSize,
-    const char* needle,
-    int needleSize);
-
-// Matches a case-insensitive prefix (up to the first needle_size bytes of
-// needle) in the first haystack_size byte of haystack. Returns a pointer past
-// the prefix, or NULL if the prefix wasn't matched.
-//
-// Always returns either NULL or haystack + needle_size.
-const char* strncaseprefix(
-    const char* haystack,
-    int haystackSize,
-    const char* needle,
-    int needleSize);
-
-// Matches a prefix; returns a pointer past the prefix, or NULL if not found.
-// (Like strprefix() and strcaseprefix() but not restricted to searching for
-// char* literals). Templated so searching a const char* returns a const char*,
-// and searching a non-const char* returns a non-const char*.
-template <class CharStar>
-inline CharStar varStrprefix(CharStar str, const char* prefix) {
-  const int len = strlen(prefix);
-  return strncmp(str, prefix, len) == 0 ? str + len : nullptr;
-}
-
-// Same as varStrprefix() (immediately above), but matches a case-insensitive
-// prefix.
-template <class CharStar>
-inline CharStar varStrcaseprefix(CharStar str, const char* prefix) {
-  const int len = strlen(prefix);
-  return strncasecmp(str, prefix, len) == 0 ? str + len : nullptr;
-}
-
-// Returns input, or "(null)" if NULL. (Useful for logging.)
-inline const char* getPrintableString(const char* const in) {
-  return nullptr == in ? "(null)" : in;
-}
-
 // Returns whether str begins with prefix.
 inline bool hasPrefixString(const StringPiece& str, const StringPiece& prefix) {
   return str.startsWith(prefix);
@@ -185,53 +73,6 @@ inline bool hasSuffixString(const StringPiece& str, const StringPiece& suffix) {
 // ? matches 0 or 1 character, while * matches 0 or more characters.
 bool matchPattern(const StringPiece& str, const StringPiece& pattern);
 
-// Returns where suffix begins in str, or NULL if str doesn't end with suffix.
-inline char* strsuffix(char* str, const char* suffix) {
-  const int lenStr = strlen(str);
-  const int lenSuffix = strlen(suffix);
-  char* strBeginningOfTheEnd = str + lenStr - lenSuffix;
-
-  if (lenStr >= lenSuffix && 0 == strcmp(strBeginningOfTheEnd, suffix)) {
-    return (strBeginningOfTheEnd);
-  }
-  return nullptr;
-}
-inline const char* strsuffix(const char* str, const char* suffix) {
-  return const_cast<const char*>(strsuffix(const_cast<char*>(str), suffix));
-}
-
-// Same as strsuffix() (immediately above), but matches a case-insensitive
-// suffix.
-char* strcasesuffix(char* str, const char* suffix);
-inline const char* strcasesuffix(const char* str, const char* suffix) {
-  return const_cast<const char*>(strcasesuffix(const_cast<char*>(str), suffix));
-}
-
-const char* strnsuffix(
-    const char* haystack,
-    int haystackSize,
-    const char* needle,
-    int needleSize);
-const char* strncasesuffix(
-    const char* haystack,
-    int haystackSize,
-    const char* needle,
-    int needleSize);
-
-// Returns the number of times a character occurs in a string for a null
-// terminated string.
-inline ptrdiff_t strcount(const char* buf, char c) {
-  if (buf == nullptr) {
-    return 0;
-  }
-  ptrdiff_t num = 0;
-  for (const char* bp = buf; *bp != '\0'; bp++) {
-    if (*bp == c) {
-      num++;
-    }
-  }
-  return num;
-}
 // Returns the number of times a character occurs in a string for a string
 // defined by a pointer to the first character and a pointer just past the last
 // character.
@@ -260,46 +101,6 @@ inline ptrdiff_t strcount(const std::string& buf, char c) {
   return strcount(buf.c_str(), buf.size(), c);
 }
 
-// Returns a pointer to the nth occurrence of a character in a null-terminated
-// string.
-// WARNING: Removes const-ness of string argument!
-char* strchrnth(const char* str, const char& c, int n);
-
-// Returns a pointer to the nth occurrence of a character in a null-terminated
-// string, or the last occurrence if occurs fewer than n times.
-// WARNING: Removes const-ness of string argument!
-char* adjustedLastPos(const char* str, char separator, int n);
-
-// STL-compatible function objects for char* string keys:
-
-// Compares two char* strings for equality. (Works with NULL, which compares
-// equal only to another NULL). Useful in hash tables:
-//    hash_map<const char*, Value, hash<const char*>, Streq> ht;
-struct Streq : public std::binary_function<const char*, const char*, bool> {
-  bool operator()(const char* s1, const char* s2) const {
-    return (
-        (s1 == nullptr && s2 == nullptr) ||
-        (s1 && s2 && *s1 == *s2 && strcmp(s1, s2) == 0));
-  }
-};
-
-// Compares two char* strings. (Works with NULL, which compares greater than any
-// non-NULL). Useful in maps:
-//    map<const char*, Value, Strlt> m;
-struct Strlt : public std::binary_function<const char*, const char*, bool> {
-  bool operator()(const char* s1, const char* s2) const {
-    return (s1 != s2) &&
-        (s2 == nullptr || (s1 != nullptr && strcmp(s1, s2) < 0));
-  }
-};
-
-// Returns whether str has only Ascii characters (as defined by asciiIsAscii()
-// in strings/ascii_ctype.h).
-bool isAscii(const char* str, int len);
-inline bool isAscii(const StringPiece& str) {
-  return isAscii(str.data(), str.size());
-}
-
 // Returns the smallest lexicographically larger string of equal or smaller
 // length. Returns an empty string if there is no such successor (if the input
 // is empty or consists entirely of 0xff bytes).
@@ -309,25 +110,6 @@ inline bool isAscii(const StringPiece& str) {
 // Examples:
 // "a" -> "b", "aaa" -> "aab", "aa\xff" -> "ab", "\xff" -> "", "" -> ""
 std::string prefixSuccessor(const StringPiece& prefix);
-
-// Returns the immediate lexicographically-following string. This is useful to
-// turn an inclusive range into something that can be used with Bigtable's
-// SetLimitRow():
-//
-//     // Inclusive range [min_element, max_element].
-//     string min_element = ...;
-//     string max_element = ...;
-//
-//     // Equivalent range [range_start, range_end).
-//     string range_start = min_element;
-//     string range_end = immediateSuccessor(max_element);
-//
-// WARNING: Returns the input string with a '\0' appended; if you call c_str()
-// on the result, it will compare equal to s.
-//
-// WARNING: Transforms "" -> "\0"; this doesn't account for Bigtable's special
-// treatment of "" as infinity.
-std::string immediateSuccessor(const StringPiece& s);
 
 // Fills in *separator with a short string less than limit but greater than or
 // equal to start. If limit is greater than start, *separator is the common
@@ -368,157 +150,8 @@ void stringReplace(
     bool replaceAll,
     std::string* res);
 
-// Replaces all occurrences of substring in s with replacement. Returns the
-// number of instances replaced. s must be distinct from the other arguments.
-//
-// Less flexible, but faster, than RE::GlobalReplace().
-int globalReplaceSubstring(
-    const StringPiece& substring,
-    const StringPiece& replacement,
-    std::string* s);
-
-// Removes v[i] for every element i in indices. Does *not* preserve the order of
-// v. indices must be sorted in strict increasing order (no duplicates). Runs in
-// O(indices.size()).
-void removeStrings(
-    std::vector<std::string>* v,
-    const std::vector<int>& indices);
-
-// Case-insensitive strstr(); use system strcasestr() instead.
-// WARNING: Removes const-ness of string argument!
-char* gstrcasestr(const char* haystack, const char* needle);
-
 // Finds (case insensitively) the first occurrence of (null terminated) needle
 // in at most the first len bytes of haystack. Returns a pointer into haystack,
 // or NULL if needle wasn't found.
 // WARNING: Removes const-ness of haystack!
 const char* gstrncasestr(const char* haystack, const char* needle, size_t len);
-char* gstrncasestr(char* haystack, const char* needle, size_t len);
-
-// Finds (case insensitively), in str (which is a list of tokens separated by
-// nonAlpha), a token prefix and a token suffix. Returns a pointer into str of
-// the position of prefix, or NULL if not found.
-// WARNING: Removes const-ness of string argument!
-char* gstrncasestrSplit(
-    const char* str,
-    const char* prefix,
-    char nonAlpha,
-    const char* suffix,
-    size_t n);
-
-// Finds (case insensitively) needle in haystack, paying attention only to
-// alphanumerics in either string. Returns a pointer into haystack, or NULL if
-// not found.
-// Example: strcasestrAlnum("This is a longer test string", "IS-A-LONGER")
-// returns a pointer to "is a longer".
-// WARNING: Removes const-ness of string argument!
-char* strcasestrAlnum(const char* haystack, const char* needle);
-
-// Returns the number times substring appears in text.
-// Note: Runs in O(text.length() * substring.length()). Do *not* use on long
-// strings.
-int countSubstring(StringPiece text, StringPiece substring);
-
-// Finds, in haystack (which is a list of tokens separated by delim), an token
-// equal to needle. Returns a pointer into haystack, or NULL if not found (or
-// either needle or haystack is empty).
-const char*
-strstrDelimited(const char* haystack, const char* needle, char delim);
-
-// Gets the next token from string *stringp, where tokens are strings separated
-// by characters from delim.
-char* gstrsep(char** stringp, const char* delim);
-
-// Appends StringPiece(data, len) to *s.
-void fastStringAppend(std::string* s, const char* data, int len);
-
-// Returns a duplicate of theString, with memory allocated by new[].
-char* strdupWithNew(const char* theString);
-
-// Returns a duplicate of up to the first maxLength bytes of theString, with
-// memory allocated by new[].
-char* strndupWithNew(const char* theString, int maxLength);
-
-// Finds, in theString, the first "word" (consecutive !asciiIsSpace()
-// characters). Returns pointer to the beginning of the word, and sets *endPtr
-// to the character after the word (which may be space or '\0'); returns NULL
-// (and *endPtr is undefined) if no next word found.
-// endPtr must not be NULL.
-const char* scanForFirstWord(const char* theString, const char** endPtr);
-inline char* scanForFirstWord(char* theString, char** endPtr) {
-  // implicit_cast<> would be more appropriate for casting to const,
-  // but we save the inclusion of "base/casts.h" here by using const_cast<>.
-  return const_cast<char*>(scanForFirstWord(
-      const_cast<const char*>(theString), const_cast<const char**>(endPtr)));
-}
-
-// For the following functions, an "identifier" is a letter or underscore,
-// followed by letters, underscores, or digits.
-
-// Returns a pointer past the end of the "identifier" (see above) beginning at
-// str, or NULL if str doesn't start with an identifier.
-const char* advanceIdentifier(const char* str);
-inline char* advanceIdentifier(char* str) {
-  // implicit_cast<> would be more appropriate for casting to const,
-  // but we save the inclusion of "base/casts.h" here by using const_cast<>.
-  return const_cast<char*>(advanceIdentifier(const_cast<const char*>(str)));
-}
-
-// Returns whether str is an "identifier" (see above).
-bool isIdentifier(const char* str);
-
-// Finds the first tag and value in a string of tag/value pairs.
-//
-// The first pair begins after the first occurrence of attributeSeparator (or
-// stringTerminal, if not '\0'); tagValueSeparator separates the tag and
-// value; and the value ends before the following occurrence of
-// attributeSeparator (or stringTerminal, if not '\0').
-//
-// Returns true (and populates tag, tagLen, value, and valueLen) if a
-// tag/value pair is founds; returns false otherwise.
-bool findTagValuePair(
-    const char* inStr,
-    char tagValueSeparator,
-    char attributeSeparator,
-    char stringTerminal,
-    char** tag,
-    int* tagLen,
-    char** value,
-    int* valueLen);
-
-// Inserts separator after every interval characters in *s (but never appends to
-// the end of the original *s).
-void uniformInsertString(std::string* s, int interval, const char* separator);
-
-// Inserts separator into s at each specified index. indices must be sorted in
-// ascending order.
-void insertString(
-    std::string* s,
-    const std::vector<uint32_t>& indices,
-    char const* separator);
-
-// Finds the nth occurrence of c in n; returns the index in s of that
-// occurrence, or string::npos if fewer than n occurrences.
-int findNth(StringPiece s, char c, int n);
-
-// Finds the nth-to-last occurrence of c in s; returns the index in s of that
-// occurrence, or string::npos if fewer than n occurrences.
-int reverseFindNth(StringPiece s, char c, int n);
-
-// Returns whether s contains only whitespace characters (including the case
-// where s is empty).
-bool onlyWhitespace(const StringPiece& s);
-
-// Formats a string in the same fashion as snprintf(), but returns either the
-// number of characters written, or zero if not enough space was available.
-// (snprintf() returns the number of characters that would have been written if
-// enough space had been available.)
-//
-// A drop-in replacement for the safe_snprintf() macro.
-int safeSnprintf(char* str, size_t size, const char* format, ...)
-    PRINTF_ATTRIBUTE(3, 4);
-
-// Reads a line (terminated by delim) from file into *str. Reads delim from
-// file, but doesn't copy it into *str. Returns true if read a delim-terminated
-// line, or false on end-of-file or error.
-bool getlineFromStdioFile(FILE* file, std::string* str, char delim);
